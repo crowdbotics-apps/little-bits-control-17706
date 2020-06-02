bcb0024f3f8e8dcc2cf7523edeefc38a06423104
Crowdbotics <team@crowdbotics.com>
2020-06-02 14:32:42 +0000

    Update models

--- little_bits_control_17706/wsgi.py
+++ little_bits_control_17706/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'little_bits_control_17706.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "little_bits_control_17706.settings")
 
 application = get_wsgi_application()
